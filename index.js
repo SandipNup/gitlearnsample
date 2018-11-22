430f57bbd7d35e3de19b39ce14015eca6745611a
Sadip Neupane <sandipneupane65@gmail.com>
2018-11-22 09:48:20 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>This is my title</title>
+</head>
+<body>
+<p id='demo'>This is the first paragaraph</p>
+<button onclick='myFunction()'>Click Me
+</button>
+<script>
+
+function myFunction()	
+	document.getElementById('demo').innerHTML=Paragraph changed;
+</script>
+</body>
+</html>
